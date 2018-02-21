8a203ae1d7127ba371b231aeb57dbc67459cd956
kimjihyeong200 <36202418+kimjihyeong200@users.noreply.github.com>
2018-02-21 10:44:44 +0900

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 sudo apt-get update 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - 
-sudo apt-get install -y nodejs 
+sudo apt-get install -y nodejs
+sudo apt-get install npm
+sudo apt-get install build-essential
+install @angular/cli
+sudo npm install -g @angular/cli
+ng new testing
+cd testing
 npm install
