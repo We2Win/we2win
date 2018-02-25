642b96264056f74ea089aff3ebd91b1da9f56382
KimJihyeong <KJH@KimJihyeongui-MacBook-Pro.local>
2018-02-26 04:49:00 +0900

    v 0.5.3 setting login Forms

--- src/app/whome/pages/login/login.component.ts
+++ src/app/whome/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { FormsModule, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms/src/validators';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +13,47 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) { }
   // we want to actually subscribe to the boolean of the observable
-  onSubmit(form: any): void {
-    console.log(form.user);
-    this.authService.login(form.user, form.pass).subscribe(auth => {
-      if (auth) {
-        this.router.navigate(['/']);
-      }
-    });
-  }
+  loginForm: FormGroup;
+  ID: FormControl;
+  Password: FormControl;
+
   ngOnInit(): void {
     this.authService.checkAuth().subscribe(auth => {
       if (auth) {
         this.router.navigate(['/']);
       }
     });
+
+    this.createForm();
+    // this.createFormControls();
+  }
+
+  createForm() {
+    this.loginForm = new FormGroup({
+      ID: this.ID,
+      Password: this.Password,
+    });
+  }
+
+  createFormControls() {
+    this.ID = new FormControl('', Validators.required);
+    this.Password = new FormControl('', [
+      Validators.required,
+      // Validators.minLength(8)
+    ]);
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.ID);
+      this.authService.login(this.loginForm.ID, this.loginForm.Password).subscribe(auth => {
+        console.log(auth);
+        if (auth) {
+          this.router.navigate(['/']);
+        }
+      });
+    } else {
+      console.log('loginForm not valid!');
+    }
   }
 }
