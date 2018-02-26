348e1e457c3f61b3cdc921a6c3001786837ae422
KimJihyeong <KJH@KimJihyeongui-MacBook-Pro.local>
2018-02-26 17:10:52 +0900

    v 0.5.9 connect php

---
+++ src/app/whome/services/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http/';
+import { User } from '../models/user';
+
+@Injectable()
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  getAll () {
+    return this.http.get<User[]>('/api');
+  }
+  getById(id: string) {
+    return this.http.get('/api');
+  }
+  create(user: User) {
+    return this.http.post('/api', user);
+  }
+  update(user: User) {
+    return this.http.put('/api', user);
+  }
+  delete(id: string) {
+    return this.http.delete('/api');
+  }
+
+}
